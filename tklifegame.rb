1a6a92bcce8e8672b8c9949087ebaca363e3ce42
Fumitaka Tokumitsu <toku345@gmail.com>
2013-08-15 12:13:44 +0900

    GUI版ライフゲーム（モデル表示）

--- tklifegame.rb
+++ tklifegame.rb
@@ -1,8 +1,8 @@
-# -*- coding: undecided -*-
+# -*- coding: utf-8 -*-
 require 'tk'
-require 'lifegame'
+require './lifegame'
 
-　# GUI版ライフゲーム本体
+# GUI版ライフゲーム本体
 class TkLifeGame
   include Tk                    # Tkの機能を取り込む
 
@@ -25,13 +25,54 @@ class TkLifeGame
 
     # [quit]ボタン生成
     @quitbutton = TkButton.new(nil, 'text' => 'quit')
-
     @canvas.pack
     @nextbutton.pack('side' => 'left')
     @gobutton.pack('side' => 'left')
     @quitbutton.pack('side' => 'left')
+
+    @prevgrid = {}
+    @rectangles = {}
+  end
+
+  # 実行
+  def run
+    display
+    mainloop
+  end
+
+  # 表示
+  def display
+    nextgrid = {}
+    @lifegame.each_life {|geom|
+      if @prevgrid[geom]
+        @prevgrid[geom] = nil     # 生き残る点
+      else
+        setrect(geom)             # 生まれた点
+      end
+      nextgrid[geom] = true
+    }
+    @prevgrid.each_key {|geom|
+      resetrect(geom)             # 死んだ点
+    }
+    @prevgrid = nextgrid
+  end
+
+  # 点の表示
+  def setrect(geom)
+    @rectangles[geom] = TkcRectangle.new(@canvas,
+                                        geom.x * @rectsize,
+                                        geom.y * @rectsize,
+                                        geom.x * @rectsize + @rectsize - 2,
+                                        geom.y * @rectsize + @rectsize - 2,
+                                        'fill' => 'black')
+  end
+
+  # 点の消失
+  def resetrect(geom)
+    @rectangles[geom].destroy
+    @rectangles[geom] = nil
   end
 end
 
 g = TkLifeGame.new
-g.mainloop
+g.run
